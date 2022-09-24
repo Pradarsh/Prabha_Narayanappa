29cda898c752669c8ccf901c3201f6b9b8fa4baa
Prabha  Narayanappa <prabhadarshan2013@gmail.com>
2022-09-24 21:22:43 +0200

    Added folder BankofNPD

---
+++ BankofNPD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankofNPD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Nia", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(2000, DateTime.Now, "Rent");
+            Console.WriteLine(account.Balance);
+            account.MakeWithdrawal(1000, DateTime.Now, "Toys");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(1000, DateTime.Now, "Handloan");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
