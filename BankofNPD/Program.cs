631d3b0af29db440706b6f4109a650baa2def1de
Prabha  Narayanappa <prabhadarshan2013@gmail.com>
2022-09-24 21:38:20 +0200

    added 00

--- BankofNPD/Program.cs
+++ BankofNPD/Program.cs
@@ -6,7 +6,7 @@ namespace BankofNPD
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Nia", 10000);
+            var account = new BankAccount("Nia", 1000000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
 
             account.MakeWithdrawal(2000, DateTime.Now, "Rent");
